41e2b35ff0513fbb7a3971b462ef05308f588d8b
Keshav Rathinavel <keshavrathinavel@gmail.com>
2025-03-11 17:10:53 +0530

    Set size for hashtable as 3gb

--- src/main.go
+++ src/main.go
@@ -8,8 +8,11 @@ import (
 )
 
 func main() {
+	println("Starting Pandora's Data Hub...")
+
+	storeSize := uint64(3 * 1024 * 1024 * 1024)
 	poolManager := storage.NewPoolManager()
-	segHashTable := storage.NewSegmentedHashTable(10, 256)
+	segHashTable := storage.NewSegmentedHashTable(16, storeSize)
 	server := internal.CreateServer(segHashTable, poolManager)
 	server.SetReady(true)
 	err := server.Start(5555)
